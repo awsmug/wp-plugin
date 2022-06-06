addb4b9405144e848639dd2c6710883e4430f5dc
Sven Wagener <sven@awesome.ug>
2022-06-06 12:18:11 +0100

    Renamded files

---
+++ example-plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Example Plugin
+ * Description: Test Plugin for WP Plugin class.
+ * Requires PHP: 7.0.0
+ * Requires at least: 6.0.0
+ * Domain Path: /langauges
+ * Text Domain: wp-plugin
+ */
+
+require dirname(__FILE__) . '/src/Plugin.php';
+require dirname(__FILE__) . '/src/PluginInfo.php';
+require dirname(__FILE__) . '/src/PluginException.php';
+
+use AWSM\WP_Plugin\Plugin;
+
+class ExamplePlugin extends Plugin {
+    public function load()
+    {
+        add_action('admin_notices', [$this, 'show_notice']);
+    }
+
+    public function show_notice() 
+    {
+        ?>
+        <div class="notice notice-success is-dismissible">
+            <p><?php _e( 'Done!', 'sample-text-domain' ); ?></p>
+        </div>
+        <?php
+    }
+}
---
+++ src/PluginException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AWSM\WP_Plugin;
+
+use Exception;
+
+/**
+ * WP Plugin Exception
+ * 
+ * @since 1.0.0
+ */
+class PluginException extends Exception {
+
+}
